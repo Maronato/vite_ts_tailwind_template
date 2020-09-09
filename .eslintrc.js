5a09818c8f7e97d50130b2d0a574cf1f44db5780
Maronato <gustavomaronato@gmail.com>
2020-09-08 23:11:08 -0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/vue",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {
+    "no-undef": "off",
+    "no-var": "error",
+    "vue/no-multiple-template-root": "off",
+    camelcase: "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { args: "all", argsIgnorePattern: "^_" },
+    ],
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/order": "error",
+  },
+}
